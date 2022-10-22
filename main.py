aef9644e8b933acacabcc536d891cf225328d815
jdlondonoc <jul.londono@gmail.com>
2022-10-21 20:56:37 -0500

    updated main.py and contact number link

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ pyscript.write('date',dt.date.today().strftime('%A %B %d, %Y'))
 
 def saludo(*args):
 	nombre = Element('name').element.value
-	Element('saludo').element.innerHTML = "Greetings "+ nombre + "! nice to having you here"
+	Element('saludo').element.innerHTML = "Greetings <strong>"+ nombre + "!</strong> Nice to having you here!<br>Could you please tell me, are you interested in Data visualization?"
+
+
+def interes(*args):
+	interesado = Element('interested').element.value
+	if interesado == "yes":
+		Element('interest').element.innerHTML = "Great! let's take a look at some examples"
+	elif interesado == "no":
+		Element('interest').element.innerHTML = "sorry about that, maybe next time"
+	else:
+		Element('interest').element.innerHTML = "please, write yes or no, thanks"
